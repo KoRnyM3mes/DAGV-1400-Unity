31011ad114388bc643f45b068568b3bd3ca4800a
Christopher Tate <CrustRust15@gmail.com>
2023-10-05 15:09:45 -0600

    Comments for code

--- UFO Defense force/Assets/scripts/PowerUp.cs
+++ UFO Defense force/Assets/scripts/PowerUp.cs
@@ -10,24 +10,24 @@ public class PowerUp : MonoBehaviour
     private float moveSpeed = 3.5f;
     void Update()
     {
-        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime); //Moves powerup down screen
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //collider for power up
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player"))//checks if game object has player tag, so only player can pick it up
         {
-            Pickup(other);
+            Pickup(other); //runs the pickup script
         }
     }
 
-    void Pickup(Collider player)
+    void Pickup(Collider player) //passes the other variable into this function
     {
-        Debug.Log("PowerUp Obtained!");
+        Debug.Log("PowerUp Obtained!"); //log message for power up pickup
 
-        PlayerController stats = player.GetComponent<PlayerController>();
-        stats.speed *= multiplier;
+        PlayerController stats = player.GetComponent<PlayerController>(); //gets speed component from player controller script
+        stats.speed *= multiplier; // applies the speed boost (perma rn)
         
-        Destroy(gameObject);
+        Destroy(gameObject);  //destroys the powerup capsule 
     }
 }
