6621928c5a8d254f42855d46a8ec41dd2bbf5281
Christopher Tate <CrustRust15@gmail.com>
2023-10-10 12:01:57 -0600

    Power up works!!! + ufo fix

--- UFO Defense force/Assets/scripts/EnemySpawnManager.cs
+++ UFO Defense force/Assets/scripts/EnemySpawnManager.cs
@@ -1,21 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawnManager : MonoBehaviour
 {
     private float spawnRangeX = 20f;
     private float spawnPositionZ = 20f;
     public GameObject[] ufoPrefabs; //array to store ufo ships
+
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomUfo", startDelay, spawnInterval);
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPOs = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
-            //spawns UFO in a random location within the bounds of the game
-            int ufoIndex = Random.Range(0,ufoPrefabs.Length);//picks random prefab in UFOs
-            Instantiate(ufoPrefabs[ufoIndex], spawnPOs, ufoPrefabs[ufoIndex].transform.rotation);
-            //instantiates a UFO from the array
-        }
+    
+    }
+
+    void SpawnRandomUfo()
+    {
+        Vector3 spawnPOs = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        //spawns UFO in a random location within the bounds of the game
+        int ufoIndex = Random.Range(0,ufoPrefabs.Length);//picks random prefab in UFOs
+        Instantiate(ufoPrefabs[ufoIndex], spawnPOs, ufoPrefabs[ufoIndex].transform.rotation);
+        //instantiates a UFO from the array 
     }
 }
