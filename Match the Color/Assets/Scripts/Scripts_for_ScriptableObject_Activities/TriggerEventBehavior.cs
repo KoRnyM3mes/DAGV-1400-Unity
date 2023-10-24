a5dcb101c57bc824c6f8761d00d56e58ba33ad08
Christopher Tate <CrustRust15@gmail.com>
2023-10-24 17:17:10 -0600

    Started scriptable object lab

---
+++ Match the Color/Assets/Scripts/Scripts_for_ScriptableObject_Activities/TriggerEventBehavior.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
