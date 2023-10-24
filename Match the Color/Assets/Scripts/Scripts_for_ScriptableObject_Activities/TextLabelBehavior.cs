4f8ae399c068367c6cb7d6049ffdff8716f0ba6a
Christopher Tate <CrustRust15@gmail.com>
2023-10-24 17:33:16 -0600

    Finish Scriptable object lab

---
+++ Match the Color/Assets/Scripts/Scripts_for_ScriptableObject_Activities/TextLabelBehavior.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextLabelBehavior : MonoBehaviour
+{
+    public Text label;
+    public FloatData dataobj;
+
+    private void Start()
+    {
+        label = GetComponent<Text>();
+        UpdateLabel();
+    }
+
+    public void UpdateLabel()
+    {
+        label.text = dataobj.value.ToString(CultureInfo.InvariantCulture);
+        
+    }
+}
