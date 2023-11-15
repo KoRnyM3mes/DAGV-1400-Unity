9bb1e446d0610ff58af3a827a06f852dd807dedb
Christopher Tate <CrustRust15@gmail.com>
2023-11-15 10:01:03 -0700

    Coroutine Assignment

---
+++ Match the Color/Assets/MatchColorAssets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
+    public IntData counterNum;
+
+    public bool canRun;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    
+    private IEnumerator Counting()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+
+        startCountEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
+        {
+            repeatCountEvent.Invoke();
+            Debug.Log(counterNum.value);
+            yield return wfsObj;
+            counterNum.value--;
+        }
+
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalseEvent.Invoke();
+        }
+    }
+}
